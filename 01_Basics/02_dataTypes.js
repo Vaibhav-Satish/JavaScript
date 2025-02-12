f95e25edf25cb99cd527906bcfb18bb6e1cab56e
Vaibhav-Satish <vaibhavsatish24@gmail.com>
2025-02-12 03:25:07 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all JS code as newer version or new JS
+
+// alert ( 3 + 3 ) // we are using NodeJs not browser
+
+console.log (3 + 3)
+console.log("Vaibhav") // code readability should be high !!!
+
+let name = "Vaibhav"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// sybmol => unique 
+
+
+// object => 
+
+console.log(typeof "Vaibhav") // gives the information or type of whats typed next to it, be ot string, object, number    
+console.log(typeof age) // OP: number
+console.log(typeof null) // OP: object
+console.log(typeof undefined) // OP: undefined
